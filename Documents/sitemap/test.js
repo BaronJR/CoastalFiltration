7b4d09d6240cde65931e4a7c45a7de6d180ec388
JReilman <jreilman@blizzard.com>
2016-05-31 13:18:52 -0700

    Cleanup directory; initial site build

--- Documents/sitemap/test.js
+++
@@ -1,9 +0,0 @@
-var views = [{"name":"James", "age":25}];
-
-
-fs = require ('fs');
-
-fs.writeFile('views.js', JSON.stringify(views), function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
-});
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # CoastalFiltration
-# sitemap
-# testsite
